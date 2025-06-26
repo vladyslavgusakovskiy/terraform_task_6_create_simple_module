8299a30a5f5f434eca2dadc82ec9009c6c4062a3
Vladyslav Gusakovskiy <vladyslav.gusakovskiy@gmail.com>
2025-06-26 12:05:55 +0300

    added tfplan; plus added a provider block to the root module

--- modules/resource_group_storage/main.tf
+++ modules/resource_group_storage/main.tf
@@ -1,3 +1,16 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "3.105.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
 resource "azurerm_resource_group" "example" {
   name     = var.resource_group_name
   location = var.location
